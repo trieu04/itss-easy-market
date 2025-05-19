36b2a1a120ede1aff56aa3da50db8e7779c00746
HexDino <hung123soaica@gmail.com>
2025-05-19 21:36:54 +0700

    Add client src and public files

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Easy Market</h1>
+        <p>Welcome to ITSS Easy Market</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
